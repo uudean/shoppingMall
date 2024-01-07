8282ea1a4ea145d4a832bf2b92fec922d943683a
uudean <swj4545@naver.com>
2024-01-07 23:42:00 +0900

    Redis + JWT 구현

---
+++ src/main/java/com/soloproject/shoppingmall/redis/RedisUtil.java
@@ -0,0 +1,50 @@
+package com.soloproject.shoppingmall.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+@Component
+public class RedisUtil {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisBlackListTemplate;
+
+    public void set(String key, Object object, long minutes) {
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(object.getClass()));
+        redisTemplate.opsForValue().set(key, object, minutes, TimeUnit.SECONDS);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public boolean delete(String key) {
+        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void setBlackList(String key, Object object, long minutes) {
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(object.getClass()));
+        redisTemplate.opsForValue().set(key, object, minutes, TimeUnit.MILLISECONDS);
+    }
+
+    public Object getBlackList(String key) {
+        return redisBlackListTemplate.opsForValue().get(key);
+    }
+
+    public boolean deleteBlackList(String key) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.delete(key));
+    }
+
+    public boolean hasKeyBlackList(String key) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
+    }
+}
